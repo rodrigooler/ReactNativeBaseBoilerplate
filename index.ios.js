39c6a5b0e98116ce109e42f09ac5a0c2f44e52ed
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-02-11 00:29:52 -0200

    Create project

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React NativeBase App
+ * https://github.com/RodrigoOler/ReactNativeBoilerplate
+ * @flow
+ */
+
+import {AppRegistry} from 'react-native';
+import ReactNativeBaseBoilerplate from './src';
+
+AppRegistry.registerComponent('ReactNativeBaseBoilerplate', () => ReactNativeBaseBoilerplate);
